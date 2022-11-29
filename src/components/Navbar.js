209688ce65283ba5959b4c1789d241f1cb1f1030
David Barath <davidbarath08@gmail.com>
2022-11-29 22:05:10 +0100

    style changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { useState, useRef } from 'react'
+
+
 //assets
 import  Logo  from "../assets/Mimi.png"
 import  Menu  from "../assets/Menu.svg"
 import  Moon  from "../assets/Moon.svg"
 import  Sun  from "../assets/Sun.svg"
-import  X  from "../assets/X.svg"
 
 
 
 
 export default function Navbar() {
-  const [isVisible, setIsVisible] =useState(true);
+  const [isActive, setIsActive] =useState(true);
   const { isDark, setIsDark } = useState(false);
   // const div = useRef();
   const handleToggle = ()=>{
-    setIsVisible(!isVisible);
+    setIsActive(!isActive);
   }
-
   
-
   return (
     <>
     <div className='md:flex md:flex-row md:justify-between justify-center items-center mb-5 hidden md:visible'>
@@ -42,6 +41,7 @@ export default function Navbar() {
         <button className='text-1xl font-medium bg-yellow px-5 py-2 w-20 rounded-full mr-20'> <img src={Moon} alt="Theme" className='w-5 mx-auto block' /> </button>
       </div>
     </div>
+    
 
     {/* mobile menu */}
       <div className='flex flex-row visible md:invisible md:hidden items-center mx-7 mb-7'>
@@ -55,19 +55,17 @@ export default function Navbar() {
       
     
     {/* mobile navbar */}
-      <div  className={isVisible ? "hidden": "inline-block w-[100%] h-[100%] relative text-center bg-stone-300 md:hidden rounded-md"}>
-      <ul className='text-2xl font-normal underline'>
-        <li className='p-5'>Home</li>
-        <li className='p-5'>Services</li>
-        <li className='p-5'>Projects</li>
-        <li className='p-5'>Contacts</li>
+      <div  className={isActive ? "hidden": "inline-block w-[100%] h-[100%] relative text-center bg-stone-100 md:hidden"}>
+      <ul className='text-2xl font-normal underline hover:d'>
+        <li className='p-3.5'>Home</li>
+        <li className='p-3.5'>Services</li>
+        <li className='p-3.5'>Projects</li>
+        <li className='p-3.5'>Contacts</li>
       </ul>
       <div>
-      <button className='w-[50%] mx-auto block bg-yellow rounded-sm'> <img src={Moon} alt="Theme" className='w-6 mx-auto block py-2'/> </button>
+      <button className='w-[25%] mx-auto block bg-yellow rounded-full mb-5'> <img src={Moon} alt="Theme" className='w-6 mx-auto block py-2'/> </button>
       </div>
     </div>
-
-    
     </>
   )
 }
